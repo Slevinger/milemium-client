0a3721486a967985a967d2b5958fad95fb3b3fa7
slevinger <shir.levinger@gmail.com>
2020-05-14 11:50:12 +0300

    Updates

---
+++ precache-manifest.6f3b73d110bf7d980134d898f9b44a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f78acdfb0fd794e810a92f6deca2b4",
+    "url": "/milenium-client/index.html"
+  },
+  {
+    "revision": "b0ff6c9b506f578ab619",
+    "url": "/milenium-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d83717d7d690dfba267d",
+    "url": "/milenium-client/static/js/2.3da304a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/milenium-client/static/js/2.3da304a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ff6c9b506f578ab619",
+    "url": "/milenium-client/static/js/main.d0aaaf38.chunk.js"
+  },
+  {
+    "revision": "b114dab0c19be17d6501",
+    "url": "/milenium-client/static/js/runtime-main.1a34bfce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/milenium-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
